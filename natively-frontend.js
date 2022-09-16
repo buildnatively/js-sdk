fc6805ddc1cec031b449c42f60027ec12cfb7fc5
Roman Furman <roman.furman@uptech.team>
2022-09-16 16:21:58 +0300

    fix start + priority (android) + interval (ios + android)

--- natively-frontend.js
+++ natively-frontend.js
@@ -323,24 +323,31 @@ class NativelyGeolocation {
 class NativelyLocation {
   constructor() {
     const id = generateID();
-    this.current = function (accuracy, location_callback) {
+    // priority_android = HIGH or BALANCED
+    // accuracy_ios = 0 - 5000 m
+    this.current = function (accuracy_ios, priority_android, location_callback) {
       window.natively.trigger(id, 3, location_callback, "location_current", {
-        accuracy,
+        accuracy: accuracy_ios,
+        priority: priority_android
       });
     };
-    this.start = function (accuracy, location_callback) {
+    this.start = function (interval, accuracy_ios, priority_android, location_callback) {
       window.natively.trigger(id, 3, location_callback, "location_start", {
-        accuracy,
+        accuracy: accuracy_ios,
+        priority: priority_android,
+        interval
       });
     };
     this.stop = function () {
       window.natively.trigger(id, 3, undefined, "location_stop", {});
     };
-    this.startBackground = function (responseIdentifier, interval, debug, location_bg_callback) {
+    this.startBackground = function (interval, accuracy_ios, priority_android, responseIdentifier, location_bg_callback) {
       const params = {};
-      params.identifier = typeof responseIdentifier === "undefined" ? "-1" : responseIdentifier;
-      params.debug = typeof debug === "undefined" ? false : debug;
+      params.identifier = typeof responseIdentifier === "undefined" ? "empty" : responseIdentifier;
+      params.debug = typeof window.natively.isDebug === "undefined" ? false : window.natively.isDebug;
       params.interval = typeof interval === "undefined" ? 1000 * 60 : interval;
+      params.accuracy = typeof accuracy_ios === "undefined" ? 50 : accuracy_ios;
+      params.priority = typeof priority_android === "undefined" ? "BALANCED" : priority_android;
       window.natively.trigger(id, 4, location_bg_callback, "location_start_bg", params);
     };
     this.stopBackground = function (location_bg_callback) {
