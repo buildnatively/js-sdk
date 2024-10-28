2856657712bba4decc792c01d0f81f43e0d21993
Roman Furman <roman.furman@uptech.team>
2024-10-28 17:59:04 +0200

    natively interface

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,28 @@
 declare global {
     interface Window {
         [key: string]: any;
-        natively: any;
+        natively: {
+            isDebug: boolean;
+            min_app_version: number;
+            app_version: number;
+            injected: boolean;
+            observers: Function[];
+            isIOSApp: boolean;
+            isAndroidApp: boolean;
+            setDebug(isDebug: boolean): void;
+            notify(min?: number, current?: number): void;
+            addObserver(fn: Function): void;
+            trigger(respId: string | undefined, minVersion: number, callback: Function | undefined, method: string, body?: any): void;
+            openLogger(): void;
+            openConsole(): void;
+            closeApp(): void;
+            showProgress(toggle: boolean): void;
+            shareImage(image_url: string): void;
+            shareText(text: string): void;
+            shareTextAndImage(text: string, image_url: string): void;
+            shareFile(file_url: string): void;
+            openExternalURL(url?: string, external?: boolean): void;
+        };
         $agent: any;
     }
 }
@@ -165,3 +186,25 @@ export declare class NativelyAppleSignInService {
     constructor();
     signin(callback: Function): void;
 }
+export declare const natively: {
+    isDebug: boolean;
+    min_app_version: number;
+    app_version: number;
+    injected: boolean;
+    observers: Function[];
+    isIOSApp: boolean;
+    isAndroidApp: boolean;
+    setDebug(isDebug: boolean): void;
+    notify(min?: number, current?: number): void;
+    addObserver(fn: Function): void;
+    trigger(respId: string | undefined, minVersion: number, callback: Function | undefined, method: string, body?: any): void;
+    openLogger(): void;
+    openConsole(): void;
+    closeApp(): void;
+    showProgress(toggle: boolean): void;
+    shareImage(image_url: string): void;
+    shareText(text: string): void;
+    shareTextAndImage(text: string, image_url: string): void;
+    shareFile(file_url: string): void;
+    openExternalURL(url?: string, external?: boolean): void;
+};
--- natively-frontend.m.js
+++ natively-frontend.m.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.NativelyStorage = exports.NativelyScanner = exports.NativelyPurchases = exports.NativelyNotifications = exports.NativelyNFCService = exports.NativelyMessage = exports.NativelyMediaPicker = exports.NativelyLocation = exports.NativelyInfo = exports.NativelyHealth = exports.NativelyGeolocation = exports.NativelyDatePicker = exports.NativelyContacts = exports.NativelyClipboard = exports.NativelyCamera = exports.NativelyBiometrics = exports.NativelyAudioRecorder = exports.NativelyAppleSignInService = exports.NativelyAdmobInterstitial = exports.NativelyAdmobBanner = void 0;
+exports.natively = exports.NativelyStorage = exports.NativelyScanner = exports.NativelyPurchases = exports.NativelyNotifications = exports.NativelyNFCService = exports.NativelyMessage = exports.NativelyMediaPicker = exports.NativelyLocation = exports.NativelyInfo = exports.NativelyHealth = exports.NativelyGeolocation = exports.NativelyDatePicker = exports.NativelyContacts = exports.NativelyClipboard = exports.NativelyCamera = exports.NativelyBiometrics = exports.NativelyAudioRecorder = exports.NativelyAppleSignInService = exports.NativelyAdmobInterstitial = exports.NativelyAdmobBanner = void 0;
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
@@ -136,7 +136,6 @@ window.natively = {
     window.natively.trigger(undefined, 18, undefined, "open_link", params);
   }
 };
-
 // Initial Setup
 window.natively.addObserver(() => window.natively.trigger(undefined, 0, resp => {
   window.natively.min_app_version = resp.minSDKVersion;
@@ -646,3 +645,4 @@ class NativelyAppleSignInService {
   }
 }
 exports.NativelyAppleSignInService = NativelyAppleSignInService;
+var natively = exports.natively = window.natively;
