9857eca7daa2000eed721e03296f73095bc49cad
Roman Furman <roman.furman@uptech.team>
2024-10-28 17:58:09 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 declare global {
   interface Window {
     [key: string]: any;
-    natively: any;
+    natively: {
+      isDebug: boolean;
+      min_app_version: number;
+      app_version: number;
+      injected: boolean;
+      observers: Function[];
+      isIOSApp: boolean;
+      isAndroidApp: boolean;
+      setDebug(isDebug: boolean): void;
+      notify(min?: number, current?: number): void;
+      addObserver(fn: Function): void;
+      trigger(
+        respId: string | undefined,
+        minVersion: number,
+        callback: Function | undefined,
+        method: string,
+        body?: any,
+      ): void;
+      openLogger(): void;
+      openConsole(): void;
+      closeApp(): void;
+      showProgress(toggle: boolean): void;
+      shareImage(image_url: string): void;
+      shareText(text: string): void;
+      shareTextAndImage(text: string, image_url: string): void;
+      shareFile(file_url: string): void;
+      openExternalURL(url?: string, external?: boolean): void;
+    };
     $agent: any;
   }
 }
@@ -161,7 +188,6 @@ window.natively = {
     window.natively.trigger(undefined, 18, undefined, "open_link", params);
   },
 };
-
 // Initial Setup
 window.natively.addObserver(() =>
   window.natively.trigger(
@@ -1058,3 +1084,5 @@ export class NativelyAppleSignInService {
     window.natively.trigger(this.id, 16, callback, "apple_signin", {});
   }
 }
+
+export const natively = window.natively;
