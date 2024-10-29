c4260a691e807390925055d8696774b5adfaa1a8
Roman Furman <roman.furman@uptech.team>
2024-10-29 13:48:45 +0200

    fix

--- natively-frontend.d.ts
+++ natively-frontend.d.ts
@@ -1,30 +1,31 @@
 declare global {
     interface Window {
         [key: string]: any;
-        natively: {
-            isDebug: boolean;
-            min_app_version: number;
-            app_version: number;
-            injected: boolean;
-            observers: Function[];
-            isIOSApp: boolean;
-            isAndroidApp: boolean;
-            setDebug(isDebug: boolean): void;
-            notify(min?: number, current?: number): void;
-            addObserver(fn: Function): void;
-            trigger(respId: string | undefined, minVersion: number, callback: Function | undefined, method: string, body?: any): void;
-            openLogger(): void;
-            openConsole(): void;
-            closeApp(): void;
-            showProgress(toggle: boolean): void;
-            shareImage(image_url: string): void;
-            shareText(text: string): void;
-            shareTextAndImage(text: string, image_url: string): void;
-            shareFile(file_url: string): void;
-            openExternalURL(url?: string, external?: boolean): void;
-        };
+        natively: Natively;
         $agent: any;
     }
+}
+interface Natively {
+    isDebug: boolean;
+    min_app_version: number;
+    app_version: number;
+    injected: boolean;
+    observers: Function[];
+    isIOSApp: boolean;
+    isAndroidApp: boolean;
+    setDebug(isDebug: boolean): void;
+    notify(min?: number, current?: number): void;
+    addObserver(fn: Function): void;
+    trigger(respId: string | undefined, minVersion: number, callback: Function | undefined, method: string, body?: any): void;
+    openLogger(): void;
+    openConsole(): void;
+    closeApp(): void;
+    showProgress(toggle: boolean): void;
+    shareImage(image_url: string): void;
+    shareText(text: string): void;
+    shareTextAndImage(text: string, image_url: string): void;
+    shareFile(file_url: string): void;
+    openExternalURL(url?: string, external?: boolean): void;
 }
 export declare class NativelyInfo {
     private id;
@@ -186,25 +187,5 @@ export declare class NativelyAppleSignInService {
     constructor();
     signin(callback: Function): void;
 }
-export declare const natively: {
-    isDebug: boolean;
-    min_app_version: number;
-    app_version: number;
-    injected: boolean;
-    observers: Function[];
-    isIOSApp: boolean;
-    isAndroidApp: boolean;
-    setDebug(isDebug: boolean): void;
-    notify(min?: number, current?: number): void;
-    addObserver(fn: Function): void;
-    trigger(respId: string | undefined, minVersion: number, callback: Function | undefined, method: string, body?: any): void;
-    openLogger(): void;
-    openConsole(): void;
-    closeApp(): void;
-    showProgress(toggle: boolean): void;
-    shareImage(image_url: string): void;
-    shareText(text: string): void;
-    shareTextAndImage(text: string, image_url: string): void;
-    shareFile(file_url: string): void;
-    openExternalURL(url?: string, external?: boolean): void;
-};
+export declare const natively: Natively;
+export {};
