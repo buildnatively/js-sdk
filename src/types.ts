e4b25d0ce5d5807e7a79aa5eedae9cfcc981a79d
Viacheslav Miroshko <ccctux@gmail.com>
2024-11-27 12:03:42 +0200

    fix(app): global classes
    # Conflicts:
    #	src/index.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,47 @@
 import {Natively} from "./classes/Natively";
+import {NativelyAdmobBanner} from "./classes/NativelyAdmobBanner";
+import {NativelyAdmobInterstitial} from "./classes/NativelyAdmobInterstitial";
+import {NativelyAppleSignInService} from "./classes/NativelyAppleSignInService";
+import {NativelyAudioRecorder} from "./classes/NativelyAudioRecorder";
+import {NativelyBiometrics} from "./classes/NativelyBiometrics";
+import {NativelyCamera} from "./classes/NativelyCamera";
+import {NativelyClipboard} from "./classes/NativelyClipboard";
+import {NativelyContacts} from "./classes/NativelyContacts";
+import {NativelyDatePicker} from "./classes/NativelyDatePicker";
+import {NativelyGeolocation} from "./classes/NativelyGeolocation";
+import {NativelyHealth} from "./classes/NativelyHealth";
+import {NativelyInfo} from "./classes/NativelyInfo";
+import {NativelyLocation} from "./classes/NativelyLocation";
+import {NativelyMediaPicker} from "./classes/NativelyMediaPicker";
+import {NativelyMessage} from "./classes/NativelyMessage";
+import {NativelyNFCService} from "./classes/NativelyNFCService";
+import {NativelyNotifications} from "./classes/NativelyNotifications";
+import {NativelyPurchases} from "./classes/NativelyPurchases";
+import {NativelyScanner} from "./classes/NativelyScanner";
+import {NativelyStorage} from "./classes/NativelyStorage";
 
 interface NativelyProps {
     natively: Natively;
+    NativelyAdmobBanner: NativelyAdmobBanner;
+    NativelyAdmobInterstitial: NativelyAdmobInterstitial;
+    NativelyAppleSignInService: NativelyAppleSignInService;
+    NativelyAudioRecorder: NativelyAudioRecorder;
+    NativelyBiometrics: NativelyBiometrics;
+    NativelyCamera: NativelyCamera;
+    NativelyClipboard: NativelyClipboard;
+    NativelyContacts: NativelyContacts;
+    NativelyDatePicker: NativelyDatePicker;
+    NativelyGeolocation: NativelyGeolocation;
+    NativelyHealth: NativelyHealth;
+    NativelyInfo: NativelyInfo;
+    NativelyLocation: NativelyLocation;
+    NativelyMediaPicker: NativelyMediaPicker;
+    NativelyMessage: NativelyMessage;
+    NativelyNFCService: NativelyNFCService;
+    NativelyNotifications: NativelyNotifications;
+    NativelyPurchases: NativelyPurchases;
+    NativelyScanner: NativelyScanner;
+    NativelyStorage: NativelyStorage;
     $agent: never;
 }
 
