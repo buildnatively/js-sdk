b4a86fcd90779d145f74127c5d56573bf54f9eac
Viacheslav Miroshko <ccctux@gmail.com>
2024-11-29 10:16:57 +0200

    fix(app): .js imports

--- dist/hooks/useNatively.js
+++ dist/hooks/useNatively.js
@@ -1,2 +1,2 @@
-import globalContext from "../utils/globalThis";
+import globalContext from "../utils/globalThis.js";
 export var useNatively = () => globalContext === null || globalContext === void 0 ? void 0 : globalContext.natively;
