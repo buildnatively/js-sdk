b23ff0fe90dcb8bc9e1993b4a98387bb117d9069
Ruslan <69873515+SkipRuslan@users.noreply.github.com>
2025-04-29 02:12:30 +0300

    fix: Add recordDataType field for NFC write event

--- src/classes/NativelyNFCService.ts
+++ src/classes/NativelyNFCService.ts
@@ -28,13 +28,14 @@ export class NativelyNFCService {
         globalContext?.natively.trigger(this.id, 15, callback, "nfc_read", params);
     }
 
-    write(recordId: string, recordData: string, callback: Function): void {
+    write(recordId: string, recordData: string, recordDataType: string, callback: Function): void {
         const params = {
             alertMessage: this.writeAlertMessage ?? "please set writeAlertMessage",
             detectedMessage:
                 this.writeDetectedMessage ?? "please set writeDetectedMessage",
             recordData: recordData ?? "please set recordData",
             recordId: recordId ?? "please set recordId",
+            recordDataType: recordDataType ?? "please set recordDataType",
         };
         globalContext?.natively.trigger(this.id, 15, callback, "nfc_write", params);
     }
