b06b874207c69b4fbc9089c30376a3163b2b22b7
SkipRuslanDev <skipruslandev@gmail.com>
2025-06-09 19:57:51 +0300

    feat: Add calendar event feature

---
+++ src/classes/NativelyCalendarEvent.ts
@@ -0,0 +1,24 @@
+import {generateID} from "../utils/generateID";
+import globalContext from "../utils/globalThis";
+
+export class NativelyCalendarEvent {
+    private readonly id: string = generateID();
+
+    createEvent(
+       title: string,
+        startDate: Date,
+        endDate?: Date,
+        timezone?: string,
+        description?: string,
+        create_calendar_event_callback?: Function,
+    ): void {
+        const params = {
+            title: title,
+            timezone: timezone,
+            description: description,
+            endDate: endDate?.toISOString(),
+            startDate: startDate.toISOString(),
+        };
+        globalContext?.natively.trigger(this.id, 0, create_calendar_event_callback, "add_calendar_event", params);
+    }
+}
