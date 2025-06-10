bb940ef0f17be8eebcf4cef3c4311277d36bbb03
SkipRuslanDev <skipruslandev@gmail.com>
2025-06-10 18:59:28 +0300

    Add retrieve calendars event

--- src/classes/NativelyCalendarEvent.ts
+++ src/classes/NativelyCalendarEvent.ts
@@ -6,19 +6,27 @@ export class NativelyCalendarEvent {
 
     createEvent(
        title: string,
-        startDate: Date,
-        endDate?: Date,
-        timezone?: string,
+       startDate: Date,
+       calendarId: string,
+       endDate?: Date,
+       timezone?: string,
         description?: string,
         create_calendar_event_callback?: Function,
     ): void {
         const params = {
             title: title,
             timezone: timezone,
+            calendarId: calendarId,
             description: description,
             endDate: endDate?.toISOString(),
             startDate: startDate.toISOString(),
         };
         globalContext?.natively.trigger(this.id, 37, create_calendar_event_callback, "calendar_event", params);
+    }
+
+    retrieveCalendars( 
+        retrieve_calendars_callback?: Function,
+    ): void {
+        globalContext?.natively.trigger(this.id, 37, retrieve_calendars_callback, "retrieve_calendars", {});
     }
 }
