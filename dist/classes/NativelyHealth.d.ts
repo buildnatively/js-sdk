c0d11c3786812eaaa78db0fbf5102c5af2f221f7
dkvyck-uptech <dmytro.kvyk@uptech.team>
2025-11-21 22:55:16 +0200

    feat: healthkit data write

--- dist/classes/NativelyHealth.d.ts
+++ dist/classes/NativelyHealth.d.ts
@@ -7,5 +7,36 @@ export declare class NativelyHealth {
     getStatisticQuantity(data_type: string, interval: string, start_date?: Date, end_date?: Date, callback?: Function): void;
     getDailySleepAnalysis(start_date?: Date, end_date?: Date, limit?: number, callback?: Function): void;
     getWorkouts(start_date?: Date, end_date?: Date, limit?: number, callback?: Function): void;
+    writeWorkout(workout: {
+        start_date?: Date;
+        end_date?: Date;
+        duration?: number;
+        activity_type?: string;
+        total_energy_burned?: number;
+        total_energy_burned_unit?: string;
+        total_distance?: number;
+        total_distance_unit?: string;
+        total_swimming_stroke_count?: number;
+        total_swimming_stroke_count_unit?: string;
+        total_flights_climbed?: number;
+        total_flights_climbed_unit?: string;
+        metadata?: any;
+        events?: any[];
+        uuid?: string;
+        identifier?: string;
+    }, callback?: Function): void;
+    writeNutrition(nutrition: {
+        items?: Array<{
+            start_date?: Date;
+            end_date?: Date;
+            [key: string]: any;
+        }>;
+        start_date?: Date;
+        end_date?: Date;
+        identifier?: string;
+        value?: number;
+        unit?: string;
+        [key: string]: any;
+    }, callback?: Function): void;
     getActivitySummary(start_date?: Date, end_date?: Date, callback?: Function): void;
 }
--- dist/classes/NativelyHealth.js
+++ dist/classes/NativelyHealth.js
@@ -1,3 +1,5 @@
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
@@ -60,6 +62,38 @@ export class NativelyHealth {
       obj.end_date = end_date.getTime();
     }
     globalContext === null || globalContext === void 0 || globalContext.natively.trigger(this.id, 21, callback, "health_get_workouts", obj);
+  }
+  writeWorkout(workout, callback) {
+    var obj = _objectSpread({}, workout);
+    if ((workout === null || workout === void 0 ? void 0 : workout.start_date) instanceof Date) {
+      obj.start_date = workout.start_date.getTime();
+    }
+    if ((workout === null || workout === void 0 ? void 0 : workout.end_date) instanceof Date) {
+      obj.end_date = workout.end_date.getTime();
+    }
+    globalContext === null || globalContext === void 0 || globalContext.natively.trigger(this.id, 39, callback, "health_write_workout", obj);
+  }
+  writeNutrition(nutrition, callback) {
+    var obj = _objectSpread({}, nutrition);
+    if ((nutrition === null || nutrition === void 0 ? void 0 : nutrition.start_date) instanceof Date) {
+      obj.start_date = nutrition.start_date.getTime();
+    }
+    if ((nutrition === null || nutrition === void 0 ? void 0 : nutrition.end_date) instanceof Date) {
+      obj.end_date = nutrition.end_date.getTime();
+    }
+    if (Array.isArray(nutrition === null || nutrition === void 0 ? void 0 : nutrition.items)) {
+      obj.items = nutrition.items.map(item => {
+        var mappedItem = _objectSpread({}, item);
+        if ((item === null || item === void 0 ? void 0 : item.start_date) instanceof Date) {
+          mappedItem.start_date = item.start_date.getTime();
+        }
+        if ((item === null || item === void 0 ? void 0 : item.end_date) instanceof Date) {
+          mappedItem.end_date = item.end_date.getTime();
+        }
+        return mappedItem;
+      });
+    }
+    globalContext === null || globalContext === void 0 || globalContext.natively.trigger(this.id, 39, callback, "health_write_nutrition", obj);
   }
   getActivitySummary(start_date, end_date, callback) {
     var obj = {};
