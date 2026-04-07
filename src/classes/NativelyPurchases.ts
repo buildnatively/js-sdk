5443717ef9c24b4d0fa8c57e2961a3ebfb964062
SkipRuslanDev <skipruslandev@gmail.com>
2026-04-07 20:34:20 +0300

    feat: add show customer center event

--- src/classes/NativelyPurchases.ts
+++ src/classes/NativelyPurchases.ts
@@ -84,4 +84,8 @@ export class NativelyPurchases {
             packageId,
         });
     }
+    
+    showCustomerCenter(show_customer_center_callback: Function): void {
+        globalContext?.natively.trigger(this.id, 42, show_customer_center_callback, "purchases_show_customer_center", {});
+    }    
 }
